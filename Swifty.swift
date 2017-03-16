69c04e97c72b94f926236c9c22ccd5a00348342a
nishanth28 <pnishanth28@gmail.com>
2017-03-15 23:46:07 -0400

    Initial-Commit

---
+++ Swifty.swift
@@ -0,0 +1,20 @@
+struct Computer{
+    
+    let company : String
+    let model : String
+    
+    init(company:String,model: model) {
+        
+        self.company = company
+        self.model = model
+        
+    }
+    
+    init(company:String){
+        
+        self.company = company
+        
+    }
+    
+    
+}
