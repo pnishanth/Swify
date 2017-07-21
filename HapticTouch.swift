8fdeb48f8f6afbe24e11c7530a3d14fc37d7194e
nishanth28 <pnishanth28@gmail.com>
2017-07-20 20:46:50 -0400

    3D-Touch

---
+++ HapticTouch.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Haptic Touch
+//
+//  Created by Nishanth P on 7/4/17.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+
+class ViewController : UIViewController {
+
+    var feedbackGen : UINotificationFeedbackGenerator?
+
+override func viewDidLoad()
+{
+    super.viewDidLoad()
+    feedbackGen = UINotificationFeedbackGenerator()
+
+    let singleTap = UITapGestureRecognizer(target:self, action:#selector(self.handleSingleTap))
+    singleTap.numberOfTouchesRequired = 1
+    singleTap.addTarget(self, action:#selector(self.handleSingleTap))
+    view.isUserInteractionEnabled = true
+    view.addGestureRecognizer(singleTap)
+}
+
+    func handleSingleTap(sender:UITapGestureRecognizer)
+    {
+        feedbackGen?.notificationOccurred(.success)
+    }
+}
