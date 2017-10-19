8215fab0d7619b20929126de9ad60907f3ef4926
Nishanth <npannerselvam@zerionsoftware.com>
2017-10-18 23:09:31 -0400

    iBeacon test

---
+++ iBeacon.swift
@@ -0,0 +1,64 @@
+//
+//
+//  iBeacon
+//
+//  Created by Nishanth P.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+}
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+}
+
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+}
+
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+}
