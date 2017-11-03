dec8cc672544e87b48aed1b243c53a2e718cfc56
Nishanth <npannerselvam@zerionsoftware.com>
2017-11-02 23:43:28 -0400

    Enum error handling

--- Enums.swift
+++ Enums.swift
@@ -4,3 +4,18 @@ enum Optional<T> {
     case none
     case something(T)
 }
+
+//Error Handling
+
+enum APIError : Error {
+    // Can't connect to the server (maybe offline?)
+    case ConnectionError(error: NSError)
+    // The server responded with a non 200 status code
+    case ServerError(statusCode: Int, error: NSError)
+    // We got no data (0 bytes) back from the server
+    case NoDataError
+    // The server response can't be converted from JSON to a Dictionary
+    case JSONSerializationError(error: Error)
+    // The Argo decoding Failed
+    case JSONMappingError(converstionError: DecodeError)
+}
