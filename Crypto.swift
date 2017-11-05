8e89064c13ec31f8bfb34de60c49c3d1ce4a8df5
Nishanth <npannerselvam@zerionsoftware.com>
2017-11-04 23:56:39 -0400

    Crypto Base

--- Crypto.swift
+++ Crypto.swift
@@ -6,5 +6,15 @@
 //  Copyright © 2017 Nishapp. All rights reserved.
 //
 
+let testStr = "Hello, world!"
+guard let baseBytes = testStr.encode(.base64) else {
+    return
+}
 
-#import
+String(validatingUTF8: baseBytes) == "SGVsbG8sIHdvcmxkIQ=="
+
+guard let unBase = baseBytes.decode(.base64) else {
+    return
+}
+
+String(validatingUTF8: unBase) == testStr
