ac57dc1fc3b21a79e89a7b703cbfabab77f5c4de
Nishanth <npannerselvam@zerionsoftware.com>
2018-03-12 02:08:29 -0400

    Script

---
+++ RunScript.swift
@@ -0,0 +1,31 @@
+//
+//
+//  Script
+//
+//  Created by Nishanth P.
+//  Copyright © 2018 Nishapp. All rights reserved.
+//
+
+
+
+import Foundation
+
+extension String {
+    func executeScript() -> String {
+        let pipe = Pipe()
+        let task = Process()
+        task.launchPath = "/bin/sh"
+        task.arguments = ["-c", String(format:"%@", self)]
+        task.standardOutput = pipe
+        let file = pipe.fileHandleForReading
+        task.launch()
+        if let result = NSString(data: file.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue) {
+            return result as String
+        }
+        else {
+            return "Error executing command"
+        }
+    }
+}
+
+print("echo Executed".executeScript()) //prints Executed.
