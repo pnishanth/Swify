2807026d90529c2a8a2d4a0d573b49d1d0e8efa9
Nishanth <npannerselvam@zerionsoftware.com>
2018-03-29 23:55:29 -0400

    ImageCrop

--- ImageCrop.swift
+++ ImageCrop.swift
@@ -6,6 +6,61 @@ func cropImage(image: UIImage, toRect: CGRect) -> UIImage? {
     return UIImage(cgImage: croppedCGImage)
 }
 
+import UIKit
+
+extension UIImage {
+    
+    func crop(to:CGSize) -> UIImage {
+        guard let cgimage = self.CGImage else { return self }
+        
+        let contextImage: UIImage = UIImage(CGImage: cgimage)
+        
+        let contextSize: CGSize = contextImage.size
+    
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        let cropAspect: CGFloat = to.width / to.height
+        
+        var cropWidth: CGFloat = to.width
+        var cropHeight: CGFloat = to.height
+        
+        if to.width > to.height { //Landscape
+            cropWidth = contextSize.width
+            cropHeight = contextSize.width / cropAspect
+            posY = (contextSize.height - cropHeight) / 2
+        } else if to.width < to.height { //Portrait
+            cropHeight = contextSize.height
+            cropWidth = contextSize.height * cropAspect
+            posX = (contextSize.width - cropWidth) / 2
+        } else { //Square
+            if contextSize.width >= contextSize.height { //Square on landscape (or square)
+                cropHeight = contextSize.height
+                cropWidth = contextSize.height * cropAspect
+                posX = (contextSize.width - cropWidth) / 2
+            }else{ //Square on portrait
+                cropWidth = contextSize.width
+                cropHeight = contextSize.width / cropAspect
+                posY = (contextSize.height - cropHeight) / 2
+            }
+        }
+        
+        let rect: CGRect = CGRectMake(posX, posY, cropWidth, cropHeight)
+        
+        // Create bitmap image from context using the rect
+        let imageRef: CGImageRef = CGImageCreateWithImageInRect(contextImage.CGImage, rect)!
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let cropped: UIImage = UIImage(CGImage: imageRef, scale: self.scale, orientation: self.imageOrientation)
+        
+        UIGraphicsBeginImageContextWithOptions(to, true, self.scale)
+        cropped.drawInRect(CGRectMake(0, 0, to.width, to.height))
+        let resized = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resized
+    }
+}
+
 extension UIImage {
     func crop(to:CGSize) -> UIImage {
         guard let cgimage = self.cgImage else { return self }
