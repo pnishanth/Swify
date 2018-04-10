682d82d4a2de477ce6f37fb82b1e8e06afd2a097
Nishanth <npannerselvam@zerionsoftware.com>
2018-04-09 23:58:37 -0400

    Autolayout constraints

--- UIView.swift
+++ UIView.swift
@@ -117,6 +117,19 @@ public extension UIView {
     
 }
 
+func constraints() {
+    let newView = UIView()
+    newView.backgroundColor = UIColor.red
+    view.addSubview(newView)
+    
+    newView.translatesAutoresizingMaskIntoConstraints = false
+    let horizontalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+    let verticalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
+    let widthConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+    let heightConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+    view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+}
+
 extension UIStackView {
 
     public convenience init(distribution: UIStackViewDistribution,
