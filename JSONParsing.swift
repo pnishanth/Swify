9c2c4e956e7bc6d794779d00d1287091984d5363
Nishanth <npannerselvam@zerionsoftware.com>
2018-04-16 23:54:50 -0400

    Json parsing

--- JSONParsing.swift
+++ JSONParsing.swift
@@ -21,3 +21,17 @@ do {
     completionHandler(nil, error)
     return
 }
+
+
+// From File
+if let path = Bundle.main.path(forResource: "test", ofType: "json") {
+    do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+        if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let person = jsonResult["person"] as? [Any] {
+            // do stuff
+        }
+    } catch {
+        // handle error
+    }
+}
