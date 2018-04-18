ec0d2b0eda8f6ae4ea392d5d2e90c043ed572da2
Nishanth <npannerselvam@zerionsoftware.com>
2018-04-17 23:55:59 -0400

    json decodable

--- JSONParsing.swift
+++ JSONParsing.swift
@@ -35,3 +35,26 @@ if let path = Bundle.main.path(forResource: "test", ofType: "json") {
         // handle error
     }
 }
+
+public protocol JSONDecodable {
+    typealias DecodableType
+    
+    static func decode(json: JSON) -> DecodableType?
+    static func decode(json: JSON?) -> DecodableType?
+    static func decode(json: [JSON]) -> [DecodableType?]
+    static func decode(json: [JSON]?) -> [DecodableType?]
+}
+public extension JSONDecodable {
+    
+    static func decode(json: JSON?) -> DecodableType? {
+        guard let json = json else { return nil }
+        return decode(json)
+    }
+    static func decode(json: [JSON]) -> [DecodableType?] {
+        return json.map(decode)
+    }
+    static func decode(json: [JSON]?) -> [DecodableType?] {
+        guard let json = json else { return [] }
+        return decode(json)
+    }
+}
